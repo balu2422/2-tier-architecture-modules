f78c6918d9773089eb5ac036cc6fc92dc423fae8
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 13:06:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "instance_type" {
 variable "db_name" {
   description = "The name of the database"
   type        = string
-  default     = "mydatabase"
+  default     = "mydatabase-module"
 }
 
 variable "db_username" {
@@ -44,6 +44,6 @@ variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
   default     = {
-    Name = "TerraformProject"
+    Name = "my-modules-resouces"
   }
 }
