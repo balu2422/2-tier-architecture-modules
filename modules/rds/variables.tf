6fab781fe5a514635265e585ce9827efe08ede57
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 10:29:46 +0530

    Create variables.tf

---
+++ modules/rds/variables.tf
@@ -0,0 +1,45 @@
+variable "name" {
+  description = "Name for the RDS instance"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where RDS will be deployed"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "Instance class for the RDS database"
+  type        = string
+}
+
+variable "db_storage" {
+  description = "Allocated storage for the RDS instance"
+  type        = number
+}
+
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_security_group_id" {
+  description = "Security group ID for the RDS instance"
+  type        = string
+}
+
+variable "db_subnet_group" {
+  description = "Subnet group name for the RDS instance"
+  type        = string
+}
