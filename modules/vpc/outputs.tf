f756f9e779407a8712419a8ccc677839874e99b2
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 11:34:33 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,17 +1,21 @@
+# modules/vpc/outputs.tf
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-  value = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
-  ]
+output "public_subnet1_id" {
+  value = aws_subnet.public1.id
 }
 
-output "private_subnet_ids" {
-  value = [
-    aws_subnet.private_subnet_1.id,
-    aws_subnet.private_subnet_2.id
-  ]
+output "public_subnet2_id" {
+  value = aws_subnet.public2.id
+}
+
+output "db_security_group_id" {
+  value = aws_security_group.rds.id
+}
+
+output "db_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
 }
