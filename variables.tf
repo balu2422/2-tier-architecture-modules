5ac7674d12593d5e12ae970189da36405f457e01
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 17:10:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "instance_type" {
 variable "db_name" {
   description = "The name of the database"
   type        = string
-  default     = "mydatabase-module"
+  default     = "myDatabaseModule"
 }
 
 variable "db_username" {
