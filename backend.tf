0d7c1bb692988bfe4eb11b0739dd5be4db635e0a
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 12:41:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "techopsbucket123"
-    key            = "vpcec2/terraform.tfstate8"
+    bucket         = "2-tier-architecture-modules"
+    key            = "tf/state"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "demo_table123"
+    dynamodb_table = "2-tier-architecture-modules-table"
   }
 }
