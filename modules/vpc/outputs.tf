c39f39f5c47833f0cdcb71c3e2c85af506e44e8e
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 11:53:00 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,17 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet1_id" {
-  value = aws_subnet.public_subnet_1.id  # Updated to match the resource name
+  value = aws_subnet.public_subnet_1.id
 }
 
 output "public_subnet2_id" {
-  value = aws_subnet.public_subnet_2.id  # Updated to match the resource name
-}
-
-output "db_security_group_id" {
-  value = aws_security_group.rds.id
-}
-
-output "db_subnet_group_name" {
-  value = aws_db_subnet_group.default.name
+  value = aws_subnet.public_subnet_2.id
 }
