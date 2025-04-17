b04cb22de42f18d650af8d1c914b1bc922015a59
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 12:08:25 +0530

    Update outputs.tf

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,9 +1,14 @@
 output "rds_endpoint" {
-  description = "Endpoint of the RDS MySQL instance"
+  description = "RDS Endpoint"
   value       = aws_db_instance.default.endpoint
 }
 
 output "rds_instance_id" {
   description = "RDS instance ID"
   value       = aws_db_instance.default.id
+}
+
+output "rds_security_group_id" {
+  description = "RDS Security Group ID"
+  value       = aws_security_group.db_sg.id
 }
