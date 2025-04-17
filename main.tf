fe7cd86d6bb9dba12030e7fd2a9d01ffc4ca9fc2
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-17 10:40:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,17 +26,3 @@ module "rds" {
   db_subnet_group    = module.vpc.db_subnet_group_name
 }
 
-output "web1_public_ip" {
-  description = "Public IP of the first EC2 instance"
-  value       = module.ec2.web1_public_ip
-}
-
-output "web2_public_ip" {
-  description = "Public IP of the second EC2 instance"
-  value       = module.ec2.web2_public_ip
-}
-
-output "rds_endpoint" {
-  description = "RDS MySQL instance endpoint"
-  value       = module.rds.rds_endpoint
-}
